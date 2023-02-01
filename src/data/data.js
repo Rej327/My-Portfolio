99964789a764e89c4d948c05fb41d30ffa7ce26c
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-02-01 17:35:39 +0800

    Adding Data

---
+++ src/data/data.js
@@ -0,0 +1,85 @@
+export const button_txt = {
+  contact: "Contact Me",
+  dl_cv: "Download my CV",
+  show_more: "Show More",
+  demo: "Demo",
+  send: "Send",
+};
+
+export const HERO = {
+  hi: "HI",
+  context: "Working...",
+};
+
+export const ABOUT_ME = {
+  head: "About Me",
+  context: "Working...",
+};
+
+export const TECHNOLOGY = [
+  {
+    icon: "sample.png",
+    skill: "HTML",
+  },
+  {
+    icon: "sample.png",
+    skill: "CSS",
+  },
+  {
+    icon: "sample.png",
+    skill: "REACT",
+  },
+  {
+    icon: "sample.png",
+    skill: "MongoDB",
+  },
+  {
+    icon: "sample.png",
+    skill: "NodeJS",
+  },
+  {
+    icon: "sample.png",
+    skill: "Tailwind CSS",
+  },
+  {
+    icon: "sample.png",
+    skill: "Filestack",
+  },
+  {
+    icon: "sample.png",
+    skill: "Render",
+  },
+  {
+    icon: "sample.png",
+    skill: "Git",
+  },
+  {
+    icon: "sample.png",
+    skill: "Github",
+  },
+  {
+    icon: "sample.png",
+    skill: "Postman",
+  },
+  {
+    icon: "sample.png",
+    skill: "Photoshop",
+  },
+];
+
+export const PROJECT = [
+  {
+    frontend: "FRONTEND",
+    image: "sample.png",
+    link: "sidereal-solar.netlify.app",
+  },
+  {
+    frontend: "FULLSTACK",
+    image: "sample.png",
+    link: "cly-blog.netlify.app",
+  },
+];
+
+export const GET_IN_TOUCH = {
+  get_in_touch: "GET IN TOUCH",
+};
