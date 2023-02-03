dc752074cbadaa157280bdf2b683d43c205feb02
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-02-03 15:47:37 +0800

    Size changes for responsive

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -6,9 +6,9 @@ const Tech = () => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
       {TECHNOLOGY.map((TECH) => (
-        <div className="technology mx-auto mt-10 grid grid-rows-1 justify-center items-center p-2">
-          <img src={TECH.icon} alt="icon" className="w-28" />
-          <h1 className="text-xl font-semibold">{TECH.skill}</h1>
+        <div className="technology sm:w-40 mx-auto mt-10 grid grid-rows-1 justify-center items-center p-2">
+          <img src={TECH.icon} alt="icon" className="w-12 md:w-28 mx-auto" />
+          <h1 className="text-base sm:text-xl font-semibold">{TECH.skill}</h1>
         </div>
       ))}
     </div>
