3f636866a2bd112a72e09e72f3e9f8290b6f79f8
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-02-03 12:51:47 +0800

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { ABOUT_ME } from "../data/data";
 import { buttonStyle, headings } from "../styles/styles";
+import "aos/dist/aos.css";
 
 const About = () => {
   return (
-    <div
-      data-aos="fade-right"
-      className="mx-auto mt-10 md:mt-0 text-center md:text-left md:mr-auto w-80"
-    >
+    <div className="mx-auto mt-10 md:mt-0 text-center md:text-left md:mr-auto w-80">
       <h1 className={headings.header}>{ABOUT_ME.head}</h1>
       <p className="text-xl md:mt-10 my-4 md:my-0">{ABOUT_ME.context}</p>
       <button className={buttonStyle.button}>{ABOUT_ME.button}</button>
